dcb136f0f2ec3508430e66069ee5d3ea2b10315e
Anthony Winters <a.r.winters@nforce-it.nl>
2016-05-14 19:21:19 +0200

    Shopping List 1.0 - Edit and Delete fixes

--- dev/shopping-list/shopping-list-item.component.ts
+++ dev/shopping-list/shopping-list-item.component.ts
@@ -5,22 +5,24 @@ import {ListItem} from "../list-item";
       selector: 'shopping-list-item',
       template: `
             <div class="input">
-                <label for="item name">Name</label>
+                <label for="item-name">Name</label>
                 <input type="text" id="item-name" [(ngModel)]="item.name">
             </div>
-            <div>
-                <label for="item amt">Amount</label>
+            <div class="input">
+                <label for="item-amt">Amount</label>
                 <input type="text" id="item-amt" [(ngModel)]="item.amount">
             </div>
-            <button class="red" (click)="onDelete()">Delete Item</button>
+            <button class="danger" (click)="onDelete()">Delete Item</button>
       `,
-      inputs: ['item']
+      inputs: ['item'],
+      outputs: ['removed']
 })
 
 export class ShoppingListItemComponent {
       item = {name: '', amount: 0};
+      removed = new EventEmitter<ListItem>();
 
       onDelete(){
-
+          this.removed.emit(this.item);
       }
 }
--- dev/shopping-list/shopping-list.component.ts
+++ dev/shopping-list/shopping-list.component.ts
@@ -21,7 +21,7 @@ import {ShoppingListItemComponent} from './shopping-list-item.component';
                   </div>
             </section>
             <section *ngIf="selectedItem != null">
-                  <shopping-list-item [item]="selectedItem"></shopping-list-item>
+                  <shopping-list-item [item]="selectedItem" (removed)="onRemove($event)"></shopping-list-item>
             </section>
   `,
   directives: [ShoppingListNewItemComponent, ShoppingListItemComponent]
@@ -32,10 +32,15 @@ export class ShoppingListComponent {
       selectedItem: ListItem;
 
       onItemAdded(item: ListItem) {
-        this.listItems.push({name: item.name ,amount: item.amount});
+        this.listItems.push({name: item.name, amount: item.amount});
       }
 
       onSelect(item: ListItem) {
         this.selectedItem = item;
+      }
+
+      onRemove(item: ListItem) {
+        this.listItems.splice(this.listItems.indexOf(item), 1);
+        this.selectedItem = null;
       }
 }
